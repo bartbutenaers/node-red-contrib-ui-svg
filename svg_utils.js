973492419076cbc10c0fdc8d68224dc183a2b080
bartbutenaers <bart.butenaers@gmail.com>
2019-08-20 22:57:30 +0200

    Contributor added

--- svg_utils.js
+++ svg_utils.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Bart Butenaers
+ * Copyright 2019 Bart Butenaers, Stephen McLaughlin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
