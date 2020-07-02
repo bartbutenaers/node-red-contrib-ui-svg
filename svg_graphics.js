eb04cb29290c214ed90ffd044dcd00150166d8b1
bartbutenaers <bart.butenaers@gmail.com>
2020-07-03 00:45:11 +0200

    Remove legacy flag

--- svg_graphics.js
+++ svg_graphics.js
@@ -411,21 +411,14 @@ module.exports = function(RED) {
                             
                             userData = JSON.parse(userData);
                             
+                            // In version 1.x.x there was a bug (msg.elementId contained the selector instead of the elementId).
+                            // This was fixed in version 2.0.0
                             var msg = {
+                                elementId  : userData.elementId,
                                 selector   : userData.selector,
                                 payload    : userData.payload,
                                 payloadType: userData.payloadType,
                                 topic      : userData.topic
-                            }
-                            
-                            // In version 1.x.x there was a bug (msg.elementId contained the selector instead of the elementId).
-                            // This was fixed in version 2.0.0, but (since it was a breaking change) by default for old nodes
-                            // we still will send the selector instead of the elementId (to avoid breaking existing flows).
-                            if ($scope.config.selectorAsElementId === undefined || $scope.config.selectorAsElementId === true) {
-                                msg.elementId = userData.selector;
-                            }
-                            else {
-                                msg.elementId = userData.elementId;
                             }
                             
                             // Get the mouse coordinates (with origin at left top of the SVG drawing)
